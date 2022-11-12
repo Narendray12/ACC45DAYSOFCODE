0e2e08ec211e8ff3ea7061d948de4ffcc226cc23
Narendray12 <95286237+Narendray12@users.noreply.github.com>
2022-11-12 22:41:45 +0530

    Add files via upload

---
+++ day 23 - odd pairs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    std::cin >> a;
+    while (a--)
+    {
+        long long n, count = 0, b = 1;
+        cin >> n;
+
+        std::cout << n * n / 2 << std::endl;
+    }
+    return 0;
+}
