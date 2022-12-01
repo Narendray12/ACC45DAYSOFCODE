7e29b0a7830cc80594cea0fbb694bdb90402d9c4
Narendray12 <95286237+Narendray12@users.noreply.github.com>
2022-12-01 09:44:40 +0530

    Add files via upload

---
+++ day 42 - minimum number of ones.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    while (a--)
+    {
+        int b;
+        cin >> b;
+        cout << b / 2 << endl;
+    }
+    return 0;
+}
